1f87c7f01bce94f75b0b4201d7bdc1ae0579feca
matiasmjcs <matiasmejiascisternas@gmail.com>
2023-11-14 18:09:10 -0300

    pages created

--- react-vite/src/pages/Applications.tsx
+++ react-vite/src/pages/Applications.tsx
@@ -1,3 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+import { getApplications } from "../services/Appications";
+import { IApplication } from "../interfaces/Application";
+import { Application } from "../components/Application";
+import { SubTitle } from "../components/SubTitle";
+import { FormApp } from "../components/FormApp";
+import BasicModal from "../components/ModalApp";
+
 export const Applications = () => {
-  return <section></section>;
+  const { data, isLoading } = useQuery({
+    queryFn: getApplications,
+    queryKey: ["applications"],
+  });
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <>
+      <article className="flex gap-2 p-2 justify-between items-center">
+        <SubTitle subtitle="your applications" />
+        <BasicModal
+          component={<FormApp text="Create Application" />}
+          text="Create Application"
+          color={"success"}
+        />
+      </article>
+      <section className="grid grid-cols-2 justify-items-center w-full h-full justify-center gap-4 my-4">
+        {data?.map((app: IApplication) => {
+          return <Application key={app.id} application={app} />;
+        })}
+      </section>
+    </>
+  );
 };
--- react-vite/src/pages/group.tsx
+++ react-vite/src/pages/group.tsx
@@ -1,3 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+import { SubTitle } from "../components/SubTitle";
+import { getGroups } from "../services/Group";
+import { IGroup } from "../interfaces/Group";
+import { GroupComponent } from "../components/Group";
+import { FormGroup } from "../components/FormGroup";
+import BasicModal from "../components/ModalApp";
+
 export const Group = () => {
-  return <section>group</section>;
+  const { data, isLoading } = useQuery({
+    queryFn: getGroups,
+    queryKey: ["groups"],
+  });
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <>
+      <article className="flex gap-2 p-2 justify-between items-center">
+        <SubTitle subtitle="your groups" />
+        <BasicModal
+          component={<FormGroup text="Create Group" />}
+          text="Create Group"
+          color={"success"}
+        />
+      </article>
+      <section className="grid grid-cols-2 justify-items-center w-full h-full justify-center gap-4 mt-4 my-4">
+        {data?.map((group: IGroup) => {
+          return <GroupComponent key={group.id} group={group} />;
+        })}
+      </section>
+    </>
+  );
 };
