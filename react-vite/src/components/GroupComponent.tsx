0ad4bbbee586bd35499118065782d98e09b137d6
matiasmjcs <matiasmejiascisternas@gmail.com>
2023-11-15 10:13:17 -0300

    update components/GroupComponent.tsx

--- react-vite/src/components/GroupComponent.tsx
+++ react-vite/src/components/GroupComponent.tsx
@@ -1,26 +1,36 @@
-import { Button } from "@mui/material";
+import { Button, IconButton } from "@mui/material";
 import { IApplication } from "../interfaces/Application";
 import { IGroup } from "../interfaces/Group";
 import BasicModal from "./ModalApp";
 import { FormGroupUpdate } from "./forms/FormGroupUpdate";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export const GroupComponent = ({ group }: { group: IGroup }) => {
   return (
     <section className="w-full max-w-2xl h-full flex flex-col border-black border-2 rounded-md p-4 gap-3 justify-between">
       <h2 className="font-bold text-lg">{group.name}</h2>
-      <section className="h-[150px] scroll-auto">
+      <section className="min-h-[180px] scroll-auto">
         {group.applications.map((app: IApplication) => {
           return (
-            <article key={app.id}>
-              <article className="flex gap-2">
-                <span className="font-semibold">Name: </span>
-                <span>{app.name}</span>
+            <section
+              className="flex justify-between bg-slate-100 rounded-md my-2"
+              key={app.id}>
+              <article>
+                <article className="flex gap-2">
+                  <span className="font-semibold">Name: </span>
+                  <span>{app.name}</span>
+                </article>
+                <article className="flex gap-2">
+                  <span className="font-semibold">Description: </span>
+                  <span>{app.description}</span>
+                </article>
               </article>
-              <article className="flex gap-2">
-                <span className="font-semibold">Description: </span>
-                <span>{app.description}</span>
+              <article className="cursor-pointer hover:scale-[1.05] transition-all">
+                <IconButton aria-label="delete" disabled color="primary">
+                  <DeleteIcon />
+                </IconButton>
               </article>
-            </article>
+            </section>
           );
         })}
       </section>
